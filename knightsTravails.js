aff1e8f1cdc900dbe3762bbd4625d0b47621fd96
danielkurtsawyer <19danielsawyer@gmail.com>
2024-05-02 09:22:03 -0400

    Implement edge finding function

--- knightsTravails.js
+++ knightsTravails.js
@@ -11,31 +11,57 @@ for(let i=0; i<8; i++){
 // function to generate possible moves, leaving from vertex i, entering the vertexes returned by the function
 const findMoves = function findPossibleKnightMoves(i){
   const possibleMoves = [];
-  const row = Math.floor(i/8);
-  const column = i % 8;
-
-  return [row, column];
+  const column = Math.floor(i/8);
+  const row = i % 8;
 
   // if any of the accesses go out of bounds (negative index, or index value undefined)
   // then it is a move that would end up off the board, so we don't push it to the possibleMoves array
 
   // try left & down
   // left 2, down 1
+  if(column-2 >= 0 && row+1 <=7){
+    possibleMoves.push(tempBoard[column-2][row+1]);
+  }
   // left 1, down 2
-
+  if(column-1 >=0 && row+2 <=7){
+    possibleMoves.push(tempBoard[column-1][row+2]);
+  }
   // try right & down
   // right 2, down 1
+  if(column+2 <= 7 && row+1 <= 7){
+    possibleMoves.push(tempBoard[column+2][row+1]);
+  }
   // right 1, down 2
+  if(column+1 <=7 && row+2 <=7){
+    possibleMoves.push(tempBoard[column+1][row+2]);
+  }
 
   // try left & up
   // left 2, up 1
+  if(column-2 >=0 && row-1 >= 0){
+    possibleMoves.push(tempBoard[column-2][row-1]);
+  }
   // left 1, up 2
+  if(column-1 >=0 && row-2 >=0){
+    possibleMoves.push(tempBoard[column-1][row-2]);
+  }
 
   // try right & up
   // right 2, up 1
+  if(column+2 <=7 && row-1 >=0){
+    possibleMoves.push(tempBoard[column+2][row-1]);
+  }
   // right 1, up 2
-  return;
+  if(column+1 >=0 && row-2 >=0){
+    possibleMoves.push(tempBoard[column+1][row-2]);
+  }
+
+  return possibleMoves;
 }
 
 // board set up
+console.log(tempBoard);
+// findMoves with all moves possible
 console.log(findMoves(26));
+// findMoves with upper move restriction - only 4 should be shown
+console.log(findMoves(24));
